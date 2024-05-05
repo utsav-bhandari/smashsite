e64ca19acec63bb09973a7fae395d3020f937269
Utsav Bhandari <utsavbhandari5040@gmail.com>
2024-05-05 17:27:42 -0500

    hotfix

--- src/playerinfo.php
+++ src/playerinfo.php
@@ -30,16 +30,16 @@ print("<h1>$playertag</h1>");
 
 $othertags = str_replace("'", "\"", $allrows[0]['all_tags']);
 print("Other tags: \n");
-foreach (json_decode($othertags) as $value) {
-    print($value . " \n");
+foreach (json_decode($othertags) as $row) {
+    print($row . " \n");
 } 
 
 print("<br>\n");
 
 print("Socials: \n");
 $socials = str_replace("'", "\"", $allrows[0]['social']);
-foreach (json_decode($socials) as $value) {
-    foreach ($value as $username) {
+foreach (json_decode($socials) as $row) {
+    foreach ($row as $username) {
         print($username . " \n");
     }
 } 
@@ -62,7 +62,7 @@ $characterlist = json_decode(str_replace("'", "\"", $allrows[0]['characters']));
 if (count((array)$characterlist) == 0) {
     print("<p>No characters found!</p>\n");
 } else {
-    foreach ((array)$characterlist as $key => $value) {
+    foreach ((array)$characterlist as $key => $row) {
         $imagename = ucfirst(str_replace("ultimate/", "", $key)) . ".png";
         print("<img src=\"../smash_images/$imagename\">");
     }
@@ -74,7 +74,11 @@ $player_id = $allrows[0]['player_id'];
 
 $tournaments = get_player_tourneys($player_id);
 foreach ($tournaments as $value) {
+<<<<<<< Updated upstream
     print("<form action='./tournamentinfo.php'>");
+=======
+    print("<form action=\"tournamentinfo.php\">");
+>>>>>>> Stashed changes
     print("<input type='hidden' name='t_name' value='{$value['cleaned_name']}'>");
     print("<input type='hidden' name='key' value='{$value['key']}'>");
     print("<input type='hidden' name='p_id' value='{$player_id}']}'>");
